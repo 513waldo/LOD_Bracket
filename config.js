89c3481a78c449f2e287a0088a97983bffa089ff
Mike Walterman <mike.walterman.513@gmail.com>
2026-06-14 18:04:04 -0400

    slow portal refresh to ten seconds

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ window.BRACKET_API_BASE_URLS = [
   "https://lod-bracket.workers.dev",
 ];
 window.BRACKET_API_BASE_URL = window.BRACKET_API_BASE_URLS[0];
-window.BRACKET_API_POLL_MS = 5000;
+window.BRACKET_API_POLL_MS = 10000;
